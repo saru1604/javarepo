c556d7f85b918f1bc2b1c9b3c72b2a5bca94eed2
saru1604 <103305844+saru1604@users.noreply.github.com>
2022-05-15 21:33:03 +0530

    Update node.ps1

--- .github/node.ps1
+++ .github/node.ps1
@@ -1,4 +1,4 @@
-git add action.yml dist/index.js node_modules/*
+git add action.yml dist/index.js node/*
 git commit -m "Use vercel/ncc"
 git tag -a -m "My first action release" v1.1
 git push --follow-tags
