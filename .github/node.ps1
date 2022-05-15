a5d6578248f25a000f503ad1f6833dff0f334338
saru1604 <103305844+saru1604@users.noreply.github.com>
2022-05-15 21:41:29 +0530

    Update and rename node.ps1 to node_modules.ps1

--- .github/node.ps1
+++
@@ -1,4 +0,0 @@
-git add action.yml dist/index.js node/*
-git commit -m "Use vercel/ncc"
-git tag -a -m "My first action release" v1.1
-git push --follow-tags
---
+++ .github/node_modules.ps1
@@ -0,0 +1,4 @@
+git add action.yml index.js node_modules/* package.json package-lock.json README.md
+git commit -m "My first action is ready"
+git tag -a -m "My first action release" v1.1
+git push --follow-tags
